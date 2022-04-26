74f85ef2cea155bc73406e937644579570bea9fa
GuilhermeTeixeira13 <guilherme.teixeira@ubi.pt>
2022-04-26 15:15:47 +0100

    .

--- HexSudoku/src/HexSudoku/FXMLDocumentController.java
+++ HexSudoku/src/HexSudoku/FXMLDocumentController.java
@@ -2,35 +2,84 @@ package HexSudoku;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.css.PseudoClass;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 
-/**
- * FXML Controller class
- *
- * @author jpc
- */
 public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    GridPane board;
+    
+    @FXML 
+    Button teste;
 
-    @FXML
-    private TextField txfladoA;
-    @FXML
-    private TextField txfladoB;
-    @FXML
-    private TextField txfladoC;
-
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        board = createBoard(board);
     }    
-
+    
     @FXML
-    private void actionCalcular(ActionEvent event) {
+    private void handleButtonAction(ActionEvent event) {
+        if (event.getSource() instanceof Button) {
+            String[][] content = getContent(board);
+            for (int row = 0; row < 16; row++) {
+                for (int col = 0; col < 16; col++){
+                    System.out.print(content[row][col]+" ");
+                }
+            System.out.print("\n");
+            }
+            System.out.println("\n\n\n");
+        }
     }
     
+    public GridPane createBoard(GridPane board){
+        for (int row = 0; row < 16; row++) {
+            for (int col = 0; col < 16; col++) {
+                TextField textField = createTextField();
+                textField.setText("2");
+                board.add(textField, row, col);
+            }
+        }
+        return board;
+    }
+    public String[][] getContent(GridPane board){
+        String[][] content = new String[16][16];
+        for (int row = 0; row < 16; row++) {
+            for (int col = 0; col < 16; col++) {
+                TextField textField = createTextField();
+                textField = (TextField) getNodeFromGridPane(board, row, col);
+                content[row][col] = textField.getText();
+            }
+        }
+        return content;
+    }
+    private TextField createTextField() {
+        TextField textField = new TextField();
+
+        // restrict input to integers:
+        textField.setTextFormatter(new TextFormatter<Integer>(c -> {
+            if (c.getControlNewText().matches("\\d?")) {
+                return c ;
+            } else {
+                return null ;
+            }
+        }));
+        return textField ;
+    }
+    private Node getNodeFromGridPane(GridPane gridPane, int row, int col) {
+    for (Node node : gridPane.getChildren()) {
+        if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
+            return node;
+        }
+    }
+    return null;
+}
 }
--- HexSudoku/src/HexSudoku/Main.java
+++ HexSudoku/src/HexSudoku/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -17,15 +18,13 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
+        scene.getStylesheets().add("fxmlview.css");
         
-        stage.setTitle("Template ...");
+        stage.setTitle("HexSudoku");
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * @param args designed without arguments.
-     */
     public static void main(String[] args) {
         launch(args);
     }
