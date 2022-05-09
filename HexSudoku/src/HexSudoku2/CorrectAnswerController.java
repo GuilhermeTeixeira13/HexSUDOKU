48bc348e3599557395875d5cea6c2a0649236433
GuilhermeTeixeira13 <guilherme.teixeira@ubi.pt>
2022-05-09 21:35:12 +0100

    adicionei a data ao ficheiro txt

--- HexSudoku/src/HexSudoku2/CorrectAnswerController.java
+++ HexSudoku/src/HexSudoku2/CorrectAnswerController.java
@@ -6,10 +6,15 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -43,8 +48,8 @@ public class CorrectAnswerController implements Initializable {
     String content;
 
     StringBuilder stringBuilder;
-    ArrayList<LocalTime> arrayTimes = new ArrayList<>();
-    ArrayList<String> tenBestTimesByOrder = new ArrayList<>();
+    ArrayList<PairsTimeDate> arrayTimesDates = new ArrayList<>();
+    ArrayList<String> tenBestTimesDatesByOrder = new ArrayList<>();
 
     private Stage stage;
     private Scene scene;
@@ -53,62 +58,91 @@ public class CorrectAnswerController implements Initializable {
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
         labeltime.setText(this.time);
-      
+
         LocalTime timeMade = LocalTime.parse(this.time, DateTimeFormatter.ofPattern("HH:mm:ss"));
-        if (timeMade.isBefore(arrayTimes.get(1))) {
-            LabelNewRecord.setVisible(true);
-            System.out.println("New record set! --> " + this.time);
+
+        if (tenBestTimesDatesByOrder.size() == 1) {
+            if (timeMade.isBefore(LocalTime.parse(tenBestTimesDatesByOrder.get(0), DateTimeFormatter.ofPattern("HH:mm:ss"))) || timeMade.compareTo(LocalTime.parse(tenBestTimesDatesByOrder.get(0), DateTimeFormatter.ofPattern("HH:mm:ss"))) == 0) {
+                LabelNewRecord.setVisible(true);
+                System.out.println("New record set! --> " + this.time);
+            } else {
+                LabelNewRecord.setVisible(false);
+            }
         } else {
-            LabelNewRecord.setVisible(false);
+            if (timeMade.isBefore(LocalTime.parse(tenBestTimesDatesByOrder.get(1), DateTimeFormatter.ofPattern("HH:mm:ss")))) {
+                LabelNewRecord.setVisible(true);
+                System.out.println("New record set! --> " + this.time);
+            } else {
+                LabelNewRecord.setVisible(false);
+            }
         }
+
     }
 
     public CorrectAnswerController(String time, String username) throws IOException {
         this.time = time;
         this.userName = username;
-
+        PairsTimeDate timeDate;
         BufferedReader reader;
+
         try {
             LocalTime timeMade = LocalTime.parse(this.time, DateTimeFormatter.ofPattern("HH:mm:ss"));
+            LocalDate DateMade = LocalDate.now();
+            SimpleDateFormat df = new SimpleDateFormat("dd-mm-yyyy");
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-mm-yyyy");
+            int linesCount = 1;
 
             reader = new BufferedReader(new FileReader("Records.txt"));
             this.stringBuilder = new StringBuilder();
             String line = null;
-            String ls = System.getProperty("line.separator");
             while ((line = reader.readLine()) != null) {
-                arrayTimes.add(LocalTime.parse(line, DateTimeFormatter.ofPattern("HH:mm:ss")));
                 stringBuilder.append(line);
-                stringBuilder.append(ls);
+                stringBuilder.append("\n");
+                linesCount++;
             }
-            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
 
+            //stringBuilder.deleteCharAt(stringBuilder.length() - 1);
             reader.close();
 
             this.content = stringBuilder.toString();
-            this.content = this.content + this.time;
-            arrayTimes.add(LocalTime.parse(timeMade.toString(), DateTimeFormatter.ofPattern("HH:mm:ss")));
+            this.content = this.content + this.time + " " + LocalDate.now().toString();
+            timeDate = new PairsTimeDate(LocalTime.parse(timeMade.toString(), DateTimeFormatter.ofPattern("HH:mm:ss")), LocalDate.now());
+            arrayTimesDates.add(timeDate);
+
+            String fileContent[] = this.content.split("\n");
+            Arrays.sort(fileContent);
+            this.content = Arrays.toString(fileContent);
+            this.content = this.content.replaceAll("\\[", "");
+            this.content = this.content.replaceAll("\\]", "");
+            this.content = this.content.replaceAll(", ", "\n");
 
             FileWriter myWriter = new FileWriter("Records.txt");
             myWriter.write(this.content);
             myWriter.close();
 
-            Collections.sort(arrayTimes);
-            tenBestTimesByOrder.clear();
-            int limite = Math.min(10, arrayTimes.size());
-            for (int i = 0; i < limite; i++) {
-                tenBestTimesByOrder.add(arrayTimes.get(i).toString());
+            tenBestTimesDatesByOrder.clear();
+            int limite = Math.min(10, linesCount);
+
+            int c = 0;
+            String teste[];
+            reader = new BufferedReader(new FileReader("Records.txt"));
+            while (((line = reader.readLine()) != null) && c < limite) {
+                teste = line.split(" ");
+                tenBestTimesDatesByOrder.add(teste[0]);
             }
+            reader.close();
+
         } catch (FileNotFoundException ex) {
         }
     }
 
     @FXML
     private void records(ActionEvent event) throws IOException {
-        System.out.println("\n10 best times:\n" + tenBestTimesByOrder.toString());
+        System.out.println("\n10 best times:\n" + tenBestTimesDatesByOrder.toString());
 
         String recordsString = "";
-        for (int i = 0; i < tenBestTimesByOrder.size(); i++) {
-            recordsString = recordsString + (i + 1) + "º -> " + tenBestTimesByOrder.get(i) + "\n";
+        for (int i = 0; i < tenBestTimesDatesByOrder.size(); i++) {
+            recordsString = recordsString + (i + 1) + "º -> " + tenBestTimesDatesByOrder.get(i) + "\n";
         }
 
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
---
+++ HexSudoku/src/HexSudoku2/PairsTimeDate.java
@@ -0,0 +1,14 @@
+package HexSudoku2;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class PairsTimeDate {
+    LocalTime time;
+    LocalDate date;
+    public PairsTimeDate(LocalTime time, LocalDate date) {
+       this.time = time;
+       this.date = date;
+    }
+}
+
